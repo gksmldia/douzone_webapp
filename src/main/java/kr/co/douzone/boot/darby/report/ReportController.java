9266f8e4769eb4936216359c53cd846c02fa2068
gksmldia <gksmldia@gmail.com>
2018-02-19 16:53:38 +0900

    계획대비 실적 프로시저 적용

--- src/main/java/kr/co/douzone/boot/darby/report/ReportController.java
+++ src/main/java/kr/co/douzone/boot/darby/report/ReportController.java
@@ -1,18 +1,24 @@
 package kr.co.douzone.boot.darby.report;
 
+import org.json.simple.JSONObject;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import kr.co.douzone.boot.darby.shipping.ShppingManagementController;
+
 @RestController
 @EnableWebMvc
 public class ReportController {
 
+	ShppingManagementController shipping = new ShppingManagementController();
+	
 	@RequestMapping(path = "/report/breeding_pig_supply_statement", method = RequestMethod.GET)
 	public View breedingPigSupplyStatement(Model model, RedirectAttributes redirect) {
 		ModelAndView mav = new ModelAndView("/report/breeding_pig_supply_statement");
@@ -32,8 +38,14 @@ public class ReportController {
 	}
 	
 	@RequestMapping(path = "/report/perfomance_chart_view", method = RequestMethod.GET)
-	public View perfomanceChartView(Model model, RedirectAttributes redirect) {
+	public View perfomanceChartView(Model model, RedirectAttributes redirect
+			,@RequestParam(value = "paramData", required = false, defaultValue = "") String paramData) {
+		JSONObject json_return = new JSONObject();
+		json_return = shipping.jsonPasing(paramData);
+		
 		ModelAndView mav = new ModelAndView("/report/perfomance_chart_view");
+		mav.addObject("paramData", json_return.toString());
+		model.addAttribute("paramData", json_return.toString());
 		return mav.getView();
 	}
 	
